06bf7418d64b8af876ad3e1e47cef679d2530dfe
Bidkar Aragon Cardenas <bidkar@cetis108.edu.mx>
2020-05-13 13:20:22 -0600

    Clase 3.4 - Formulario Login
    - Se definió el formulario de inicio de sesión utilizando bulma

--- public/login.php
+++ public/login.php
@@ -1,23 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MyApp - Inicio de sesión</title>
-    <link rel="stylesheet" href="css/bulma.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>MyApp - Inicio de sesión</title>
+  <link rel="stylesheet" href="css/bulma.css">
 </head>
+
 <body>
-    <section class="hero is-light is-fullheight">
-        <div class="hero-body">
-            <div class="columns">
-                <div class="column is-4">
-                    <div class="container">
-                        <h1 class="title">Bienvenido a MyApp</h1>
-                        <h2 class="subtitle">Inicia de sesión</h2>
-                    </div>
+  <section class="hero is-dark is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <div class="columns">
+          <div class="column is-4 is-offset-4">
+            <div class="box">
+              <form action="#" method="post">
+                <div class="field has-text-centered">
+                  <img src="images/logo-cetis108.png" alt="Logo">
+                  <h3 class="is-size-4 has-text-weight-bold">Bienvenido a MyApp</h3>
                 </div>
+                <hr>
+                <div class="field">
+                  <label class="label">Usuario</label>
+                  <div class="control">
+                    <input
+                      type="text"
+                      name="usuario"
+                      id="usuario"
+                      class="input"
+                      placeholder="Escribe tu nombre de usuario"
+                    >
+                    <p class="help">El nombre de usuario que te proporcionaron en la escuela.</p>
+                  </div>
+                </div>
+                <div class="field">
+                  <label class="label">Contraseña</label>
+                  <div class="control">
+                    <input
+                      type="password"
+                      name="contrasenia"
+                      id="contrasenia"
+                      class="input"
+                      placeholder="Escribe la contraseña"
+                    >
+                  </div>
+                </div>
+                <div class="field">
+                  <div class="control">
+                    <button type="submit" class="button is-fullwidth is-link">Iniciar sesión</button>
+                  </div>
+                </div>
+              </form>
             </div>
+          </div>
         </div>
-    </section>
+      </div>
+    </div>
+  </section>
 </body>
+
 </html>
